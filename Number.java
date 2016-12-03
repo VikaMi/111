3c9e2398d3f98336e681b5dd3fb9294c1020821f
VikaMi <viktoria.mishchanyn@gmail.com>
2016-12-03 14:34:42 +0200

    added Number

---
+++ Number.java
@@ -0,0 +1,18 @@
+public class Number {
+
+
+    private int n;
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    @Override
+    public String toString() {
+        return "" + n;
+    }
+}
